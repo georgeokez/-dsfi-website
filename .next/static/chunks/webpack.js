d15b773eb3091863e07bb85e087c4f667c1d777f
georgeokez <georgeokez@gmail.com>
2022-12-04 20:07:18 +0000

    chore: minor cleanup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "77f2bc3669ed5d6f7486";
+/******/ 	var hotCurrentHash = "3fb6e9feaed355f571ee";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is the DSFI Website
+node version 14
