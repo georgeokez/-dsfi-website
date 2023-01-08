6340fbeb665aba69a0c13d28f0c56f2836676cb8
georgeokez <georgeokez@gmail.com>
2023-01-08 20:50:01 +0000

    chore: fix mobile menu

--- pages/ContactPage/index.js
+++ pages/ContactPage/index.js
@@ -1,23 +1,21 @@
-import React, {Fragment} from 'react';
-import Header from '../../components/header';
-import PageTitle from '../../components/pagetitle';
-import Footer from '../../components/footer';
-import Scrollbar from '../../components/scrollbar';
-import MetaHead from '../../components/MetaHead';
-import PageHeader from '../../components/PageHeaders/PageHeaders';
-import { contactDetials, socialAccounts } from '../../components/utils/contents';
-
+import React, { Fragment } from "react";
+import Header from "../../components/header";
+import PageTitle from "../../components/pagetitle";
+import Footer from "../../components/footer";
+import Scrollbar from "../../components/scrollbar";
+import MetaHead from "../../components/MetaHead";
+import PageHeader from "../../components/PageHeaders/PageHeaders";
 
 const ContactUsPage = () => {
-    return(
-        <Fragment>
-            <MetaHead />
-            <Header/>
-            <PageTitle pageTitle={'Contact Us'} pagesub={'Contact Us'}/>
-            <PageHeader imageUrl={'images/contact_us.png'} />
-            <Footer/>
-            <Scrollbar/>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <MetaHead />
+      <Header />
+      <PageTitle pageTitle={"Contact Us"} pagesub={"Contact Us"} />
+      <PageHeader imageUrl={"images/contact_us.png"} />
+      <Footer />
+      <Scrollbar />
+    </Fragment>
+  );
 };
 export default ContactUsPage;
