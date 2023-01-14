5c26092f974fb6a56d5fdd1906822829217b3b5e
georgeokez <georgeokez@gmail.com>
2023-01-14 09:33:35 +0000

    chore: added more volunteer jobs

--- components/hero2/index.js
+++ components/hero2/index.js
@@ -1,33 +1,42 @@
-import { paymentLink } from '../utils/contents';
+import { paymentLink } from "../utils/contents";
 
 const Hero2 = (props) => {
-
-    return(
-        <section className="hero hero-style-1">
-            <div className="hero-slider">
-                <div className="slide">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col col-lg-6 slide-caption">
-                                <div className="slide-title">
-                                    <h2>We Believe in <span>promoting</span></h2>
-                                </div>
-                                <div className="slide-subtitle">
-                                    <p>Individuals and families to speak out on their circumstances and/or challanges</p>
-                                    <p>And we provide case management services to communities that share in our mission and vision to help Individuals and families.</p>
-                                </div>
-                                <div className="btns">
-                                    <a className="theme-btn" href={paymentLink} target="_blank">Donate Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="right-vec">
-                    </div>  
+  return (
+    <section className="hero hero-style-1">
+      <div className="hero-slider">
+        <div className="slide">
+          <div className="container">
+            <div className="row">
+              <div className="col col-lg-6 slide-caption">
+                <div className="slide-title">
+                  <h2>
+                    We Believe in <span>encouraging</span>
+                  </h2>
                 </div>
+                <div className="slide-subtitle">
+                  <p>
+                    Individuals and families to speak out on their circumstances
+                    and/or challanges
+                  </p>
+                  <p>
+                    And we provide services and programs services to communities
+                    that share in our mission and vision to help Individuals and
+                    families.
+                  </p>
+                </div>
+                <div className="btns">
+                  <a className="theme-btn" href={paymentLink} target="_blank">
+                    Donate Now
+                  </a>
+                </div>
+              </div>
             </div>
-        </section>
-    )
-}
+          </div>
+          <div className="right-vec"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero2;
