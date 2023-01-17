d2698062eaa204ed0dac62251e696c2ac8937026
georgeokez <georgeokez@gmail.com>
2023-01-17 23:44:46 +0000

    chore: final updates

--- pages/index.js
+++ pages/index.js
@@ -5,28 +5,22 @@ import Hero2 from "../components/hero2";
 import Mission from "../components/mission";
 import About from "../components/about";
 import CaseSlide from "../components/case";
-import CounterSection from "../components/counter";
 import TeamSection from "../components/team";
 import WorldSection from "../components/world";
-import BlogSection from "../components/BlogSection";
 import Footer from "../components/footer";
 import Scrollbar from "../components/scrollbar";
-import { ToastContainer } from "react-toastify";
 
 const HomePage = () => {
   return (
     <Fragment>
       <MetaHead />
-      {/* <ToastContainer/>*/}
       <Header />
       <Hero2 />
       <Mission subclass={"section-padding"} />
       <About />
       <CaseSlide />
-      {/* <CounterSection/> */}
       <TeamSection />
       <WorldSection />
-      {/* <BlogSection/> */}
       <Footer />
       <Scrollbar />
     </Fragment>
